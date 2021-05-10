414c329518a48ba4756abcd57dd792f75873cdda
Serhat YILMAZ <srhtyilmaz19@gmail.com>
2021-05-10 18:41:24 +0300

    Edit file name

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const dotenv = require("dotenv");
 dotenv.config({ path: "./config.env" });
 const app = require("./app");
-require("./database/index");
+require("./database/database");
 
 const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
